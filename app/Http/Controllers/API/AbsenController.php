f3287e9896a96181cbe04275689970eed5ee2d92
mamanssuparman <63030102+mamanssuparman@users.noreply.github.com>
2024-06-08 11:04:57 +0700

    fix(absen): perbaikan waktu sesuai data local server

--- app/Http/Controllers/API/AbsenController.php
+++ app/Http/Controllers/API/AbsenController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use DateTime;
 use App\Models\User;
 use App\Models\Kehadiran;
 use Illuminate\Http\Request;
@@ -15,6 +16,7 @@ use App\Exceptions\ValidasiException;
 class AbsenController extends Controller
 {
     public function absenmasuk(Request $request){
+        date_default_timezone_set('Asia/Jakarta');
         try {
             $validasi = $request->validate([
                 'latitudeinput'     => 'required',
@@ -28,6 +30,8 @@ class AbsenController extends Controller
             if(!$dataAbsensi){
                 throw new \Exception("Data master absensi belum di setting.");
             }
+            $masterWaktuAbsen = date($dataAbsensi->jam_masuk);
+            $absenSekarang = date('H:i:s');
             $latitudeMaster = $dataAbsensi->latitude;
             $longitudeMaster = $dataAbsensi->langitude;
             $jarak = DistanceHelper::haversineGreatCircleDistance($request->input('latitudeinput'),$request->input('langitudeinput'), $latitudeMaster, $longitudeMaster);
@@ -46,8 +50,13 @@ class AbsenController extends Controller
                 'latitude_masuk'    => $request->latitudeinput,
                 'longitude_masuk'   => $request->langitudeinput,
                 'foto_masuk'        => $filename,
-                'jarak_absen_masuk' => $jarak
+                'jarak_absen_masuk' => $jarak,
+                'master_waktu_masuk'=> $dataAbsensi->jam_masuk,
+                'master_waktu_keluar'   => $dataAbsensi->jam_keluar
             ];
+            if($absenSekarang > $masterWaktuAbsen){
+                $data['status_absen_masuk']='Terlambat';
+            }
             Kehadiran::create($data);
             return ResponseFormatter::success(['jarak'  => $jarak], 'Absen masuk berhasil di simpan');
         } catch (Exception $error) {
@@ -60,6 +69,7 @@ class AbsenController extends Controller
         }
     }
     public function absenkeluar(Request $request){
+        date_default_timezone_set('Asia/Jakarta');
         try {
             //ambil data absensi terakhir
             $dataAbsenMasuk = Kehadiran::where('pegawais_id', auth()->user()->id)->where('tgl_absensi', date('y-m-d'))->whereNull('waktu_keluar')->first();
@@ -111,6 +121,7 @@ class AbsenController extends Controller
         // $data = User::with(['kehadiran'])->whereHas('kehadiran', function($query){
         //     $query->where('tgl_absensi', date('Y-m-d'));
         // })->get();
+        date_default_timezone_set('Asia/Jakarta');
         $tanggal = date('Y-m-d');
         $data = User::with(['kehadiran'=> function($query) use ($tanggal){
             $query->where('tgl_absensi', $tanggal);
